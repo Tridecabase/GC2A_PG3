0d661e63222cd537d001aea6cfec1c533574befc
Stargazerxiii-design <78391698+Tridecabase@users.noreply.github.com>
2025-05-29 15:09:14 +0900

    update

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<iostream>
+#include<string>
 
 template<typename T>
 class NumberCompare 
@@ -17,20 +18,19 @@ T a, b;
 };
 
 int main() {
-NumberCompare<int> intNint(5, 10);
-NumberCompare<int> intNfloat(5, 10.0f);
-NumberCompare<int> intNdouble(5, 10.0);
-NumberCompare<float> floatNfloat(5.0f, 10.0f);
-NumberCompare<double> floatNdouble(5.0, 10.0);
-NumberCompare<double> doubleNdouble(5.0, 10.0);
+NumberCompare<int> intNint(1, 13);
+NumberCompare<int> intNfloat(10.0f, 5);
+NumberCompare<int> intNdouble(7, 18.0);
+NumberCompare<float> floatNfloat(3.1f, 4.5f);
+NumberCompare<double> floatNdouble(5.0f, 5.1);
+NumberCompare<double> doubleNdouble(123.0, 321.0);
 
-std::cout << "Min of 5 and 10: " << intNint.Min(5, 10) << std::endl;
-std::cout << "Min of 5 and 10.0f: " << intNfloat.Min(5, 10.0f) << std::endl;
-std::cout << "Min of 5 and 10.0: " << intNdouble.Min(5, 10.0) << std::endl;
-std::cout << "Min of 5.0f and 10.0f: " << floatNfloat.Min(5.0f, 10.0f) << std::endl;
-std::cout << "Min of 5.0 and 10.0: " << floatNdouble.Min(5.0, 10.0) << std::endl;
-std::cout << "Min of 5.0 and 10.0: " << doubleNdouble.Min(5.0, 10.0) << std::endl;
-
+std::cout << "Min of 1 and 13: " << std::to_string(intNint.Min(1, 13)) << std::endl;
+std::cout << "Min of 10.0f and 5: " << std::to_string(intNfloat.Min(10.0f, 5)) << std::endl;
+std::cout << "Min of 7 and 18.0: " << std::to_string(intNdouble.Min(7, 18.0)) << std::endl;
+std::cout << "Min of 3.1f and 4.5f: " << std::to_string(floatNfloat.Min(3.1f, 4.5f)) << std::endl;
+std::cout << "Min of 5.0f and 5.1: " << std::to_string(floatNdouble.Min(5.0f, 5.1)) << std::endl;
+std::cout << "Min of 123.0 and 321.0: " << std::to_string(doubleNdouble.Min(123.0, 321.0)) << std::endl;
 
 return 0;
 }
