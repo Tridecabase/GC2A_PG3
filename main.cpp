decfd45a3ccc8707e7483ef6e8ec9934fa741fb3
Stargazerxiii-design <78391698+Tridecabase@users.noreply.github.com>
2025-06-27 14:56:41 +0900

    creat

--- main.cpp
+++ main.cpp
@@ -1,26 +1,7 @@
 ﻿#include<stdio.h>
 #include<iostream>
-#include"iShape.h"
-#include"circle.h"
-#include"rectangle.h"
 
 int main() {
-
-	// Circleとのインスタンスを生成
-	IShape* shape1 = new Circle(5.0f);
-	// サイズを計算
-	shape1->size();
-	// サイズを表示
-	shape1->draw();
-	// Rectangleとのインスタンスを生成
-	IShape* shape2 = new Rectangle(4.0f, 6.0f);
-	// サイズを計算
-	shape2->size();
-	// サイズを表示
-	shape2->draw();
-	// メモリを解放
-	delete shape1;
-	delete shape2;
 
 return 0;
 }
