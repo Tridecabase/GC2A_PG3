6969e7209383b7addb23555913dea3453c5c07b9
Stargazerxiii-design <78391698+Tridecabase@users.noreply.github.com>
2025-07-18 12:20:19 +0900

    Refactor main.cpp to use threads for output
    Replaced the student number file reading and sorting logic with a demonstration of concurrent output using three threads. Removed unused includes and deleted students.txt as it is no longer needed.

--- main.cpp
+++ main.cpp
@@ -1,60 +1,34 @@
 ﻿#include <stdio.h>
-#include <iostream>
-#include <vector>
-#include <fstream>
-#include <string>
-#include <regex>
+#include <thread>
 
 using namespace std;
 
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 //
-// 学生番号を読み込み、ソートして表示する
+// threadで並行処理、THREAD1とTHREAD2とTHREAD3を文字に出力する
 //
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 
+void PrintThread1() {
+	printf("THREAD1\n");
+}
+
+void PrintThread2() {
+	printf("THREAD2\n");
+}
+
+void PrintThread3() {
+	printf("THREAD3\n");
+}
+
 int main(int argc, char const* argv[]) {
 
-	// 学生番号を格納するための配列
-	vector<string> students;
-	// 学生番号を読み込むファイル
-	FILE* fp = nullptr;
-	errno_t err = fopen_s(&fp, "students.txt", "r");
-	if (err != 0 || fp == nullptr) {
-		cerr << "fail to open file\r\n";
-		return 1;
-	}
-	// 学生番号の読み込み
-	char buf[256];
-	while (fscanf_s(fp, " \"%255[^\"]\"%*[,] ", buf, (unsigned)sizeof(buf)) == 1){
-		if (buf[0] == '\0') {
-			continue;
-		}
-		students.push_back(buf);
-	}
-
-    fclose(fp);
-
-
-	cout << "元の学生番号" << string(20, ' ') << "ソート後の学生番号" << "\r\n";
-	// 学生番号の最大数
-	size_t max_count = students.size();
-	// ソート後の学生番号を格納するための配列
-	vector<string> sorted_students = students;
-	// 学生番号をソート
-	sort(sorted_students.begin(), sorted_students.end());
-
-	// 学生番号を表示（左：元、右：ソート後）
-	for (size_t i = 0; i < max_count; ++i) {
-		string student = students[i];
-		string sorted_student = sorted_students[i];
-		cout << student << string(10, ' ') << "\033[31m" << sorted_student << "\033[0m" << "\r\n";
-	}
-
-	// 学生の総数
-	cout << "\r\n学生の総数: " << max_count << "\r\n";
-
-	delete[] fp; // ファイルポインタの解放
+	std::thread thread1(PrintThread1);
+	thread1.join();
+	std::thread thread2(PrintThread2);
+	thread2.join();
+	std::thread thread3(PrintThread3);
+	thread3.join();
 
 	return 0;
 }
